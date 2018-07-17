716847a52095075195a7288e2176e336fd975532
Austin Atchley <austinatchley@gmail.com>
2018-07-16 21:19:46 -0700

    Add Gdx import for exit button

--- core/src/me/austinatchley/States/GameOverState.java
+++ core/src/me/austinatchley/States/GameOverState.java
@@ -1,5 +1,6 @@
 package me.austinatchley.States;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
